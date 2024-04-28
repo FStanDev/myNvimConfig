f0dbb15cbe26cb1507321feab1c05c035f631359
FStanDev <116457798+FStanDev@users.noreply.github.com>
2024-04-28 07:45:04 +0200

    Change default theme to nightfox

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -39,12 +39,12 @@ return {
 		end,
 	},
 	{
-		"oxfist/night-owl.nvim",
+		"EdenEast/nightfox.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd.colorscheme("night-owl")
+			vim.cmd.colorscheme("Duskfox")
 		end,
 	},
 	{
