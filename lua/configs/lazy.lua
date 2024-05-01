95f8167dade773692c33114381aa63ba6f298614
FStanDev <116457798+FStanDev@users.noreply.github.com>
2024-05-01 18:02:23 +0200

    Solve typo with duskfox theme

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -44,7 +44,7 @@ return {
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd.colorscheme("Duskfox")
+			vim.cmd.colorscheme("duskfox")
 		end,
 	},
 	{
