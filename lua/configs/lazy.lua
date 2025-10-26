d902fd14b69c454d5a2a005b24abfe47bcc1b7a1
FStanDev <116457798+FStanDev@users.noreply.github.com>
2025-10-26 17:29:49 +0100

    Add autocompletion for ([{ characters plugin

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -83,6 +83,25 @@ return {
 		end,
 	},
 	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			local autopairs = require("nvim-autopairs")
+			autopairs.setup({
+				check_ts = true, -- Enable treesitter
+				ts_config = {
+					lua = { "string" }, -- Don't add pairs in lua string treesitter nodes
+					javascript = { "template_string" }, -- Don't add pairs in javscript template_string
+				},
+			})
+
+			-- Integration with nvim-cmp
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
+	{
 		"stevearc/conform.nvim",
 		event = { "BufReadPre", "BufNewFile" },
 		opts = function()
